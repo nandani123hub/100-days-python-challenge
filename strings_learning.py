8407e65aa82ffa12d7d9f60e7e239a18f97fa7cf
Nandani <nandanim555@gmail.com>
2026-03-15 22:13:44 +0530

    string learning

---
+++ strings_learning.py
@@ -0,0 +1,20 @@
+name = "Nandani"
+print("Hello", name)
+print('Hi, "I want to learn python properly"')
+a = """Hi,
+I am Nandani,
+I am learning python."""
+print(a)
+
+#print the words of string
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+#print all the words of string using for loop
+for newa in a:
+    print(newa)
