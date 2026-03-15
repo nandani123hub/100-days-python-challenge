618e105acd96d73e6479894ae76dd2edf43d8f2d
Nandani <nandanim555@gmail.com>
2026-03-15 22:25:35 +0530

    added new line for if and not  check length and character in the present string

--- strings_learning.py
+++ strings_learning.py
@@ -26,3 +26,20 @@ print(len(name))
 #To check if a certain phrase or character is present in a string
 txt = "The best things in life are free!"
 print("free" in txt)
+
+
+#Use it in an if statement:
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+#To check if a certain phrase or character is NOT present in a string
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+#Use it in an if statement:
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
